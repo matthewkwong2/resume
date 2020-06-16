3f8882bcd7114820f8f30df765bef37bc748bf6a
kmw111 <kmw111@ha.org.hk>
2020-06-16 16:19:53 +0800

    Updates

--- src/actions/actions.js
+++
@@ -1,6 +0,0 @@
-import { DUMMY_ACTION } from './actionTypes';
-
-export const dummyAction = () => ({
-  type: DUMMY_ACTION,
-  payload: {}
-});
--- src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import dummyState from './dummyState';
-
-export default combineReducers({ dummyState });
