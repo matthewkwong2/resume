5fa95dcd8f62e1be6b3e3f50e65055bedc27f6a2
kmw111 <kmw111@ha.org.hk>
2020-06-30 14:05:04 +0800

    [Feature] Observe useSelector hook

--- src/wdyr.js
+++ src/wdyr.js
@@ -3,6 +3,9 @@ import React from 'react';
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
-    trackAllPureComponents: true
+    trackAllPureComponents: true,
+    trackExtraHooks: [
+      [require('react-redux/lib'), 'useSelector']
+    ],
   });
 }
