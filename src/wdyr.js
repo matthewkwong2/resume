855c2b4c1d2216dd06b0882ae7a72c6fcb5ba253
kmw111 <kmw111@ha.org.hk>
2020-07-01 12:07:50 +0800

    [Refactor]

--- src/wdyr.js
+++ src/wdyr.js
@@ -3,9 +3,6 @@ import React from 'react';
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
-    trackAllPureComponents: true,
-    trackExtraHooks: [
-      [require('react-redux/lib'), 'useSelector']
-    ],
+    trackAllPureComponents: true
   });
 }
