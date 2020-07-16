94cb8bcf6f348d2d5afaace7f6d4c99d52196600
kmw111 <kmw111@ha.org.hk>
2020-07-16 09:05:43 +0800

    [Feature] Include import preact in if condition

--- src/wdyr.js
+++ src/wdyr.js
@@ -1,7 +1,6 @@
-import { h } from 'preact';
-
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  const { h } = require('preact');
   whyDidYouRender(h, {
     trackAllPureComponents: true
   });
