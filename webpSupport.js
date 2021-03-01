be263537a3fd1ce6538e9372a4ace008aa3e7338
kmw111 <kmw111@ha.org.hk>
2021-03-01 09:51:43 +0800

    [Optimization] Change webp support detection; [Feature] Add analytsis

---
+++ webpSupport.js
@@ -0,0 +1,22 @@
+const { getUserAgentRegExp } = require('browserslist-useragent-regexp');
+
+console.log(getUserAgentRegExp({
+  browsers: [
+    "edge >= 18",
+    "Firefox >= 65",
+    "Chrome >= 9",
+    "Safari >= 14",
+    "Opera >= 11.5",
+    "iOS >= 14.4",
+    "OperaMini all",
+    "Android >= 4",
+    "OperaMobile >= 12",
+    "ChromeAndroid >= 32",
+    "FirefoxAndroid  65",
+    "UCAndroid >= 12.12",
+    "Samsung >= 4",
+    "QQAndroid >= 10.4",
+    "Baidu >=7.12"
+  ],
+  allowHigherVersions: true
+}));
